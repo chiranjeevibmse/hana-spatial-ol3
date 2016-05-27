abd6b04d3dfb722ffca4d90140c04beb5947cc75
Chiranjeevi GK <chiranjeevibmse@gmail.com>
2016-05-27 12:47:32 +0530

    added skeleton of the UI5 project

---
+++ hana-spatial-osm/WebContent/Component.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], function (UIComponent) {
+	"use strict";
+
+	return UIComponent.extend("maptest.Component", {
+
+		metadata : {
+			manifest: "json"
+		},
+
+		init : function () {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// additional initialization can be done here
+		}
+
+	});
+});
